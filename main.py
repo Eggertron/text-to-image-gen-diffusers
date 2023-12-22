77677ace1f81db83a65aeef4cb10b451924bf39a
Egger Haaaaan <bboyhan@hotmail.com>
2023-12-22 12:06:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# Import libraries
+import torch
+from diffusers import DiffusionPipeline
+
+parser = argparse.ArgumentParser(prog='Simple text to image diffuser')
+parser.add_argument('prompt', required=True)
+args = parser.parse_args()
+
+# Set model checkpoint path
+model_path = "model.ckpt"
+
+# Initialize the pipeline
+pipe = DiffusionPipeline.from_pretrained(model_path)
+
+# Define your text prompt
+text_prompt = args.prompt
+
+# Generate the image
+image = pipe.text_to_image(text_prompt, guidance_scale=7.5)
+
+# Save or display the image
+image.save("output.png")
+# or 
+#image.show()
